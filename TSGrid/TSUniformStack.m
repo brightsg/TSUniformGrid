5fe801c77dfed6b163d59147220cd2cff7051d44
Jonathan Mitchell <jonathan@mugginsoft.com>
2014-02-05 11:07:56 +0000

    Fixed subview autolayout issue.

--- TSGrid/TSUniformStack.m
+++ TSGrid/TSUniformStack.m
@@ -97,7 +97,6 @@
 
 - (void)addSubview:(NSView *)view withLayout:(BOOL)doLayout
 {
-    view.translatesAutoresizingMaskIntoConstraints = NO;
     [self.stackViews addObject:view];
     
     [super addSubview:view];
@@ -157,6 +156,14 @@
 
 - (void)resizeSubviewsWithOldSize:(NSSize)oldSize
 {
+    // the arithmetic involved in setting out the view frames manually is simpler
+    // than computing the neccessary Auto Layout constraints.
+    
+    // NOTE: When doing custom layout using setFrame it is important to ensure that -translatesAutoresizingMaskIntoConstraints
+    // returns YES for the manually placed views. If this is not ensured than the layout will misbehave.
+    //
+    // ref: https://developer.apple.com/library/ios/documentation/userexperience/conceptual/AutolayoutPG/AdoptingAutoLayout/AdoptingAutoLayout.html#//apple_ref/doc/uid/TP40010853-CH15-SW1
+    
     NSSize size = self.bounds.size;
     CGFloat x = 0;
     CGFloat y = 0;
